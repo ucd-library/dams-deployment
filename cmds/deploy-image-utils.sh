aa495aaf7517d8e68e3c876e26b3dcba64d06c0c
Justin Merz <jrmerz@gmail.com>
2023-02-15 11:27:37 -0800

    adding workflow container

---
+++ cmds/deploy-image-utils.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+
+set -e
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $ROOT_DIR/..
+source config.sh
+
+gcloud config set project $PROJECT_ID
+
+gcloud beta run deploy $IMAGE_UTILS_CLOUD_RUN_SERVICE_NAME \
+  --image $IMAGE_UTILS_IMAGE_NAME \
+  --platform managed \
+  --memory=4Gi \
+  --region=us-central1
