6e7d706d06927141688723c0574900af535b1fda
Justin Merz <jrmerz@gmail.com>
2023-03-10 09:41:44 -0800

    using fin for local build

--- cmds/build-local-dev.sh
+++ cmds/build-local-dev.sh
@@ -8,14 +8,18 @@
 
 set -e
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $ROOT_DIR
+cd $ROOT_DIR/..
+
+source config.sh
 
 export LOCAL_DEV=true
 
-./build.sh
+# build local fin
+$REPOSITORY_DIR/$FIN_SERVER_REPO_NAME/devops/build.sh 
+
+./cmds/build.sh
 
 # now build keycloak
-source $ROOT_DIR/../config.sh
 docker build \
   -t $KEYCLOAK_IMAGE_NAME:$APP_VERSION \
   $ROOT_DIR/../keycloak
