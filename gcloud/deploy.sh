bbfda31d0cc0724eba8f175f6aa45f410b3ede64
Justin Merz <jrmerz@gmail.com>
2023-03-13 14:27:35 -0700

    another fix for auto-deployment

--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-REMOTE_EXEC="ssh -i /ci-bot/.ssh/gcb ci-bot@sandbox.dams.library.ucdavis.edu"
+REMOTE_EXEC="ssh -i /root/.ssh/gcb ci-bot@sandbox.dams.library.ucdavis.edu"
 REMOTE_DIR="/opt/dams-${BRANCH_NAME}"
 
 count=$($REMOTE_EXEC "ls $REMOTE_DIR" | wc -l)
