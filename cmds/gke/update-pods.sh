3b71a1346a3e3234ba7ecec3ceb93e66e0b4498b
Justin Merz <jrmerz@gmail.com>
2024-05-28 10:59:11 -0700

    adding new import deployments for gke

--- cmds/gke/update-pods.sh
+++ cmds/gke/update-pods.sh
@@ -24,7 +24,6 @@ kubectl apply -k ./kustomize/pg-rest/base
 # fin services
 kubectl apply -k ./kustomize/fin/gateway/overlays/$ENV
 kubectl apply -k ./kustomize/fin/dbsync/overlays/$ENV
-kubectl apply -k ./kustomize/fin/fin-cache/overlays/$ENV
 kubectl apply -k ./kustomize/fin/uber/overlays/$ENV
 kubectl apply -k ./kustomize/fin/workflow/overlays/$ENV
 kubectl apply -k ./kustomize/fin/gcs/overlays/$ENV
