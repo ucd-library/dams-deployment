c47ecc123e124b189b11e44b9b9e1fd042929a86
Justin Merz <jrmerz@gmail.com>
2024-09-25 14:33:41 -0700

    update for collection import

---
+++ cmds/sync-kustomize-gcs.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $ROOT_DIR/..
+
+gcloud config set project ucdlib-dams
+gsutil cp -r kustomize/* gs://dams-kustomize-templates/
