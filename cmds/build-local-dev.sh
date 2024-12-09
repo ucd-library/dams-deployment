f488ad4fc6093aa40fa600f5dcfa8882b66a5edb
Justin Merz <jrmerz@gmail.com>
2024-12-09 11:05:17 -0800

    fix for local-dev builds

--- cmds/build-local-dev.sh
+++ cmds/build-local-dev.sh
@@ -4,4 +4,8 @@ DEPTH=$1
 if [[ ! -z "$DEPTH" ]]; then
   DEPTH="--depth $DEPTH"
 fi
-cork-kube build exec -p dams -v sandbox -o sandbox $DEPTH
+cork-kube build exec \
+  -p dams \
+  -v sandbox \
+  --no-cache-from \
+  -o sandbox $DEPTH
